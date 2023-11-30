b3594f528f6fce64ae02fb66bb1c67c22b283d91
Norhanahmedsaber <norhan3182001@gmail.com>
2023-11-30 13:36:23 +0200

    parsing config

--- src/utils/parsingconfig.mjs
+++ src/utils/parsingconfig.mjs
@@ -1,6 +1,7 @@
 import extract from "../extractor/extractor.mjs";
 
 export default function parseConfig() {
-    const config = extract('../../hanterconfig.json')
+    const config = extract('hanterconfig.json')
+    console.log(config)
     return JSON.parse(config)
 }
