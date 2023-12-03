e113b94eaf97383b290c4c43b38726dd3049b86b
Mukhtar82 <muhammedmukhtar822@gmail.com>
2023-12-03 11:07:20 +0200

    create evaluate function in the matcher

--- src/index.mjs
+++ src/index.mjs
@@ -4,9 +4,11 @@ import getFiles from './file_traverser/file_traverser.mjs'
 import extract from './extractor/extractor.mjs'
 import reporter from './reporter/reporter.mjs'
 import parseConfig from './utils/parsingconfig.mjs'
+import evaluate from './matcher/evaluate.mjs'
 
 export default function hanter(){
     const config = parseConfig()
     
-    console.log(getFiles('./src', config))
+    //console.log(getFiles('./src', config))
+
 }
---
+++ src/matcher/evaluate.mjs
@@ -0,0 +1,41 @@
+
+export default function evaluate(logicObject) {
+
+    // Base case: if the type is 'pattern', return the value directly
+    if (logicObject.type === 'pattern') {
+        return logicObject.value;
+    }
+
+    // Recursive case: evaluate each element in the 'value' array
+    const evaluatedValues = logicObject.value.map(evaluate);
+
+    // Apply the logic gate operation
+    if (logicObject.type === 'AND') {
+        return evaluatedValues.every(Boolean);
+    } else if (logicObject.type === 'OR') {
+        return evaluatedValues.some(Boolean);
+    } else {
+        throw new Error(`Unknown logic gate type: ${logicObject.type}`);
+    }
+}
+
+// // Example usage
+// const x= {
+//     type: 'OR',
+//     value: [
+//         { type: 'AND', value: [
+//             { type: 'pattern', value: true },
+//             { type: 'pattern', value: false }
+//         ]},
+//         { type: 'AND', value: [
+//             { type: 'pattern', value: true },
+//             { type: 'OR', value: [
+//                 { type: 'pattern', value: false },
+//                 { type: 'pattern', value: true }
+//             ]}
+//         ]}
+//     ]
+// };
+
+// const result = evaluate(x);
+// console.log('Result:', result); 
