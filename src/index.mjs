8d52700c2ac037b9a4c34806f2de3026fd3c6e0a
Hussien Mostafa <hussienmostafa888@gmail.com>
2023-12-03 12:59:38 +0200

    Create errors enum and report functuon

--- src/index.mjs
+++ src/index.mjs
@@ -5,10 +5,14 @@ import extract from './extractor/extractor.mjs'
 import reporter from './reporter/reporter.mjs'
 import parseConfig from './utils/parsingconfig.mjs'
 import evaluate from './matcher/evaluate.mjs'
+import report from './error/error.mjs'
+import {Errors} from './error/error.mjs'
+
+
 
 export default function hanter(){
     const config = parseConfig()
-    
     //console.log(getFiles('./src', config))
 
+    report(Errors.error1)
 }
--- src/matcher/evaluate.mjs
+++ src/matcher/evaluate.mjs
@@ -19,23 +19,39 @@ export default function evaluate(logicObject) {
     }
 }
 
-// // Example usage
-// const x= {
-//     type: 'OR',
-//     value: [
-//         { type: 'AND', value: [
-//             { type: 'pattern', value: true },
-//             { type: 'pattern', value: false }
-//         ]},
-//         { type: 'AND', value: [
-//             { type: 'pattern', value: true },
-//             { type: 'OR', value: [
-//                 { type: 'pattern', value: false },
-//                 { type: 'pattern', value: true }
-//             ]}
-//         ]}
-//     ]
-// };
 
-// const result = evaluate(x);
-// console.log('Result:', result); 
+
+// Example usage
+const x= {
+    type: 'AND',
+    value: [
+        { type: 'OR', value: [
+            { type: 'pattern', value: false },
+            { type: 'AND', value: [
+                { type: 'pattern', value: true },
+                { type: 'OR', value: [
+                    { type: 'pattern', value: false },
+                    { type: 'pattern', value: true }
+                ]}
+            ]}
+        ]},
+        { type: 'OR', value: [
+            { type: 'pattern', value: false },
+            { type: 'AND', value: [
+                { type: 'pattern', value: true },
+                { type: 'pattern', value: false }
+            ]}
+        ]},
+        { type: 'AND', value: [
+            { type: 'pattern', value: true },
+            { type: 'pattern', value: true },
+            { type: 'OR', value: [
+                { type: 'pattern', value: false },
+                { type: 'pattern', value: true }
+            ]}
+        ]}
+    ]
+};
+
+const result = evaluate(x);
+console.log('Result:', result); 
