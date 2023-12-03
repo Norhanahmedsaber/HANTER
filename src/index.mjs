c16481212a6050c15322964ffa97458a830e541e
Norhanahmedsaber <norhan3182001@gmail.com>
2023-12-03 11:26:29 +0200

    rulesParser(Noraweanas)

--- src/index.mjs
+++ src/index.mjs
@@ -4,9 +4,16 @@ import getFiles from './file_traverser/file_traverser.mjs'
 import extract from './extractor/extractor.mjs'
 import reporter from './reporter/reporter.mjs'
 import parseConfig from './utils/parsingconfig.mjs'
+import parseRule from './rules_parser/rules_parser.mjs'
 
 export default function hanter(){
-    const config = parseConfig()
-    
-    console.log(getFiles('./src', config))
+    console.log(parseRule({
+        id: 'open-never-closed',
+        message: 'file $X object opened without corresponding close',
+        languages: [ 'python' ],
+        severity: 'ERROR',
+        patterns: [
+            { patterns: [ { pattern: 'x = 2' }, { pattern: null } ] },
+            { 'patterns-either': [ { pattern: null }, { pattern: null } 
+        ] }]}).patterns[0].patterns)
 }
--- src/utils/parsingconfig.mjs
+++ src/utils/parsingconfig.mjs
@@ -2,6 +2,5 @@ import extract from "../extractor/extractor.mjs";
 
 export default function parseConfig() {
     const config = extract('hanterconfig.json')
-    console.log(config)
     return JSON.parse(config)
 }
