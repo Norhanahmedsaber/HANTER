e2bbcc923c00197e37a232339d54ecd272690ceb
Norhanahmedsaber <norhan3182001@gmail.com>
2024-01-04 17:11:40 +0200

    template literal

---
+++ test/functionExpression.js
@@ -0,0 +1,3 @@
+ // const func_2 = function nora () {console.log("Hello i am a function");} (init) id is nora and unused
+
+// const func_2 = function () {console.log("Hello i am a function");} id is null
